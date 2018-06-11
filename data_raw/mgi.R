bf20bc1f22b5601f57f243e638865148a9449c12
Chris Stubben <chris.stubben@gmail.com>
2018-06-11 10:25:08 -0600

    update mgi.R

--- data_raw/mgi.R
+++ data_raw/mgi.R
@@ -2,7 +2,7 @@
 
 # http://www.informatics.jax.org/mgihome/projects/overview.shtml
 
-## download May 22, 2017
+## download May 22, 2018
 
 ## last database update
 ## 05/15/2018
@@ -12,16 +12,23 @@ url <- "http://www.informatics.jax.org/downloads/reports/HMD_HumanPhenotype.rpt"
 # add empty column 8 to avoid parsing failures
 message("Downloading ", url)
 x <- read_tsv(url, col_names=c("human", "entrez_gene", "homologene", "x1",  "mouse", "id", "phenotype_id", "x2"))
-## 408 duplicated mouse genes
-message("Duplicated mouse gene?")
-print(table(duplicated(x$mouse)))
-## group by gene names
+## duplicated mouse genes
+table(duplicated(x$mouse))
+# FALSE  TRUE
+# 18118   407
+
+# Group by gene names
 mgi <- group_by(x, id, mouse) %>% summarize(n=n(), human = paste(human, collapse=",")) %>%
  ungroup() %>% arrange( grepl("Rik$", mouse), mouse)
 # 307 genes with 2 or more homologs
-message("Genes with 2 or more homologs")
-print(filter(mgi, n>1))
+filter(mgi, n>1)
+# A tibble: 307 x 4
+#   id          mouse       n human
+#   <chr>       <chr>   <int> <chr>
+# 1 MGI:2385289 Acsm2       2 ACSM2A,ACSM2B
+# 2 MGI:87921   Adh1        3 ADH1A,ADH1B,ADH1C
+# 3 MGI:87984   Akp3        3 ALPG,ALPI,ALPP
+
 attr(mgi, "downloaded") <- Sys.Date()
 attr(mgi, "version") <- "6.12"
-message("Saved mgi.rda to data/")
-save(mgi, file="../data/mgi.rda")
+save(mgi, file="mgi.rda")
