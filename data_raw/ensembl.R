9b720bc1650f081bccbb039ee84aa441252827ba
Chris Stubben <chris.stubben@gmail.com>
2018-10-24 10:23:41 -0600

    add release 94

--- data_raw/ensembl.R
+++ data_raw/ensembl.R
@@ -1,37 +1,45 @@
 ## Ensembl annotations
 
-mouse90 <- read_biomart_version("mouse", version=90)
-mouse92 <- read_biomart_version("mouse", version=92)
+    mouse94 <- read_biomart("mouse")
+    human94 <- read_biomart("human")
+      rat94 <- read_biomart("rat")
+      fly94 <- read_biomart("fly")
+    sheep94 <- read_biomart("sheep")
+      pig94 <- read_biomart("pig")
+   rabbit94 <- read_biomart("rabbit")
+     worm94 <- read_biomart("worm")
+zebrafish94 <- read_biomart("zebrafish")
 
-human90 <- read_biomart_version("human", version=90)
-human92 <- read_biomart_version("human", version=92)
 
-rat90 <- read_biomart_version("rat", version=90)
-rat92 <- read_biomart_version("rat", version=92)
+mouse90 <- read_biomart("mouse", version=90)
+human90 <- read_biomart("human", version=90)
+rat90 <- read_biomart("rat", version=90)
+fly90 <- read_biomart("fly", version=90)
+sheep90 <- read_biomart("sheep", version=90)
+pig90 <- read_biomart("pig", version=90)
+rabbit90 <- read_biomart("rabbit", version=90)
+worm90 <- read_biomart("worm", version=90)
+zebrafish90 <- read_biomart("zebrafish", version=90)
 
-fly90 <- read_biomart_version("fly", version=90)
-fly92 <- read_biomart_version("fly", version=92)
 
-sheep90 <- read_biomart_version("sheep", version=90)
-sheep92 <- read_biomart_version("sheep", version=92)
+mouse92 <- read_biomart("mouse", version=92)
+human92 <- read_biomart("human", version=92)
+rat92 <- read_biomart("rat", version=92)
+fly92 <- read_biomart("fly", version=92)
+sheep92 <- read_biomart("sheep", version=92)
+pig92 <- read_biomart("pig", version=92)
+rabbit92 <- read_biomart("rabbit", version=92)
+worm92 <- read_biomart("worm", version=92)
+zebrafish92 <- read_biomart("zebrafish", version=92)
 
-pig90 <- read_biomart_version("pig", version=90)
-pig92 <- read_biomart_version("pig", version=92)
-
-rabbit90 <- read_biomart_version("rabbit", version=90)
-rabbit92 <- read_biomart_version("rabbit", version=92)
-
-worm90 <- read_biomart_version("worm", version=90)
-worm92 <- read_biomart_version("worm", version=92)
-
-zebrafish90 <- read_biomart_version("zebrafish", version=90)
-zebrafish92 <- read_biomart_version("zebrafish", version=92)
-
-yeast90 <- read_biomart_version("yeast", version=90)
-yeast92 <- read_biomart_version("yeast", version=92)
 
 ### ADD human homologs to mmu
 data(mgi)
+
+mouse94$human_homolog <- ""
+n <- match(mouse94$gene_name, mgi$mouse)
+mouse94$human_homolog <- mgi$human[n]
+
 
 mouse92$human_homolog <- ""
 n <- match(mouse92$gene_name, mgi$mouse)
@@ -52,7 +60,7 @@ save(  pig90, file =   "pig90.rda")
 save(rabbit90, file="rabbit90.rda")
 save(zebrafish90, file = "zebrafish90.rda")
 save( worm90, file =  "worm90.rda")
-save(yeast90, file = "yeast90.rda")
+
 
 save(mouse92, file = "mouse92.rda")
 save(human92, file = "human92.rda")
@@ -63,9 +71,18 @@ save(  pig92, file =   "pig92.rda")
 save(rabbit92, file="rabbit92.rda")
 save(zebrafish92,file="zebrafish92.rda")
 save( worm92, file =  "worm92.rda")
-save(yeast92, file = "yeast92.rda")
 
 
+save(mouse94, file = "mouse94.rda")
+save(human94, file = "human94.rda")
+save(  rat94, file =   "rat94.rda")
+save(  fly94, file =   "fly94.rda")
+save(sheep94, file = "sheep94.rda")
+save(  pig94, file =   "pig94.rda")
+save(rabbit94, file="rabbit94.rda")
+save(zebrafish94, file = "zebrafish94.rda")
+save( worm94, file =  "worm94.rda")
+save(yeast94, file = "yeast94.rda")
 
 ## Human homologs in Mouse
 
@@ -83,7 +100,7 @@ x <- filter(x, perc_id> 35)
 
 ## GET all homologs with max percent identity and combine into a comma-delimited list
 
-mouse92_homologs <- group_by(x, id, mouse, perc_id) %>%
+mouse92_homologs <- group_by(x, id, mouse) %>%
       filter(perc_id == max(perc_id)) %>%    #
        group_by(id, mouse, perc_id) %>%
         summarize(n=n_distinct(human), human = paste(unique(human), collapse=",")) %>% ungroup()
--- data_raw/mgi.R
+++ data_raw/mgi.R
@@ -2,11 +2,12 @@
 
 # http://www.informatics.jax.org/mgihome/projects/overview.shtml
 
+
 ## download May 22, 2018
 
 ## last database update
-## 05/15/2018
-## MGI 6.12
+## 10/09/2018
+# MGI 6.12
 
 url <- "http://www.informatics.jax.org/downloads/reports/HMD_HumanPhenotype.rpt"
 # add empty column 8 to avoid parsing failures
@@ -15,19 +16,20 @@ x <- read_tsv(url, col_names=c("human", "entrez_gene", "homologene", "x1",  "mou
 ## duplicated mouse genes
 table(duplicated(x$mouse))
 # FALSE  TRUE
-# 18118   407
+# 18117   269
 
 # Group by gene names
 mgi <- group_by(x, id, mouse) %>% summarize(n=n(), human = paste(human, collapse=",")) %>%
  ungroup() %>% arrange( grepl("Rik$", mouse), mouse)
 # 307 genes with 2 or more homologs
 filter(mgi, n>1)
-# A tibble: 307 x 4
+# A tibble: 198 x 4
 #   id          mouse       n human
 #   <chr>       <chr>   <int> <chr>
 # 1 MGI:2385289 Acsm2       2 ACSM2A,ACSM2B
 # 2 MGI:87921   Adh1        3 ADH1A,ADH1B,ADH1C
-# 3 MGI:87984   Akp3        3 ALPG,ALPI,ALPP
+# 3 MGI:87984   Akp3        2 ALPG,ALPI
+# 4 MGI:1915111 Akr1b10     2 AKR1B10,AKR1B15
 
 attr(mgi, "downloaded") <- Sys.Date()
 attr(mgi, "version") <- "6.12"
